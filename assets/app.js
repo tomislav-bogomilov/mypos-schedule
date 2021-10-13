8a1ce162e6ba9f2d5fe04fb1eb2591db08a1a8b2
Tommy <t.bogomilov@gmail.com>
2021-10-13 14:19:26 +0300

    Add calendar

--- assets/app.js
+++ assets/app.js
@@ -7,6 +7,10 @@ import { render } from 'react-dom';
 import ScheduleApp from './js/Components/ScheduleApp';
 import AppointmentsTable from './js/Components/AppointmentsTable';
 import AppointmentForm from './js/Components/AppointmentForm';
+import FullCalendar from '@fullcalendar/react';
+import timeGridPlugin from '@fullcalendar/timegrid';
+import dayGridPlugin from '@fullcalendar/daygrid';
+import calendarBootstrap from '@fullcalendar/bootstrap';
 
 const predicate = 'awesome';
 
@@ -20,6 +24,21 @@ if (document.getElementById('appointments-table')) {
 
 if (document.getElementById('make-appointment-form')) {
     render(<AppointmentForm/>, document.getElementById('make-appointment-form'));
+}
+
+if (document.getElementById('main-dashboard')) {
+    render(
+        <FullCalendar
+            plugins={[ timeGridPlugin, calendarBootstrap]}
+            initialView="timeGridWeek"
+            weekends={false}
+            events={[
+                { title: 'event 1', date: '2021-10-12 12:10:10' },
+                { title: 'event 2', date: '2019-04-02' }
+            ]}
+        />,
+        document.getElementById('main-dashboard')
+    );
 }
 
 
